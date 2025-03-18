0e254c5a0e7f2bef16387bb5722058761b52f166
shubham-mhatre <mhatreshubham03@gmail.com>
2025-03-18 15:33:21 +0530

    project setup

---
+++ src/main/java/com/sm/mt/SpringbootJdbcMultitenantApplication.java
@@ -0,0 +1,13 @@
+package com.sm.mt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcMultitenantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbcMultitenantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/mt/SpringbootJdbcMultitenantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.mt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcMultitenantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
